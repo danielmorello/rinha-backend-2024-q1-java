8ed3d586ab911f332cf8ad2a4e6c9bf26ae4d50b
Daniel Morello <danielmorello@gmail.com>
2024-02-22 00:31:49 -0300

    criação da relação dos clientes e transações

--- src/main/java/br/com/dsm/RinhaBackend/domain/user/model/User.java
+++ src/main/java/br/com/dsm/RinhaBackend/domain/user/model/User.java
@@ -1,10 +1,16 @@
 package br.com.dsm.RinhaBackend.domain.user.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.dsm.RinhaBackend.domain.transaction.model.Transaction;
 import jakarta.annotation.Nonnull;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -28,4 +34,6 @@ public class User {
 
   private Integer limite;
   private Integer saldo;
+  @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<Transaction> transacoes = new ArrayList<>();
 }
