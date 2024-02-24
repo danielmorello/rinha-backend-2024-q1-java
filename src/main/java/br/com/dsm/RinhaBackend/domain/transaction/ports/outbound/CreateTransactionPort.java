2f0f0d265a292893cf8856be62ec69e346b6ddf9
Daniel Morello <danielmorello@gmail.com>
2024-02-24 17:22:42 -0300

    criação do camada de serviço da criação da transação

--- src/main/java/br/com/dsm/RinhaBackend/domain/transaction/ports/outbound/CreateTransactionPort.java
+++ src/main/java/br/com/dsm/RinhaBackend/domain/transaction/ports/outbound/CreateTransactionPort.java
@@ -3,6 +3,6 @@ package br.com.dsm.RinhaBackend.domain.transaction.ports.outbound;
 import br.com.dsm.RinhaBackend.domain.transaction.model.Transaction;
 
 public interface CreateTransactionPort {
-  // TODO: Refazer os tipos e os nomes das funções para adaptar para as transações
-  void createTransaction(Transaction transaction);
+	// TODO: Refazer os tipos e os nomes das funções para adaptar para as transações
+	void createTransaction(Transaction transaction);
 }
--- src/main/java/br/com/dsm/RinhaBackend/domain/transaction/services/CreateTransactionService.java
+++ src/main/java/br/com/dsm/RinhaBackend/domain/transaction/services/CreateTransactionService.java
@@ -5,22 +5,34 @@ import br.com.dsm.RinhaBackend.domain.transaction.mapper.TransactionMapper;
 import br.com.dsm.RinhaBackend.domain.transaction.model.Transaction;
 import br.com.dsm.RinhaBackend.domain.transaction.ports.inbound.CreateTransactionUseCase;
 import br.com.dsm.RinhaBackend.domain.transaction.ports.outbound.CreateTransactionPort;
+import br.com.dsm.RinhaBackend.domain.user.model.User;
+import br.com.dsm.RinhaBackend.domain.user.ports.outbound.FindUserPort;
+import java.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CreateTransactionService implements CreateTransactionUseCase {
-  // TODO: Refazer os tipos e os nomes das funções para adaptar para as transações
 
-  @Autowired
-  private TransactionMapper transactionMapper;
+	// TODO: Refazer os tipos e os nomes das funções para adaptar para as transações
 
-  @Autowired
-  private CreateTransactionPort createtransactionPort;
+	@Autowired
+	private TransactionMapper transactionMapper;
 
-  @Override
-  public void createTransaction(TransactionDto transactionDto) {
-    Transaction transaction = transactionMapper.toTransaction(transactionDto);
-    createtransactionPort.createTransaction(transaction);
-  }
+	@Autowired
+	private CreateTransactionPort createTransactionPort;
+
+	@Autowired
+	private FindUserPort findUserPort;
+
+	@Override
+	public void createTransaction(Integer clientId, TransactionDto transactionDto) {
+		// TODO: verificar se as transacoes sao do tipo e somente do tipo 'd' ou 'c'
+		// TODO: verificar a necessidades das exceptions para os tipos de dados na entrada
+		Transaction transaction = transactionMapper.toTransaction(transactionDto);
+		User user = findUserPort.findUser(clientId).orElseThrow();
+		transaction.setCliente(user);
+		transaction.setRealizada_em(LocalDateTime.now());
+		createTransactionPort.createTransaction(transaction);
+	}
 }
