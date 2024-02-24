6216376885a9e4fbe4a885d0bcabce27e4c902c0
Daniel Morello <danielmorello@gmail.com>
2024-02-24 17:24:29 -0300

    tentativa de iniciar o banco do users

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO 'user' ('id', 'limite', 'saldo') VALUES (1, 1000 * 100, 0);
+INSERT INTO 'user' ('id', 'limite', 'saldo') VALUES (2, 1000 * 100, 0);
+INSERT INTO 'user' ('id', 'limite', 'saldo') VALUES (3, 1000 * 100, 0);
+INSERT INTO 'user' ('id', 'limite', 'saldo') VALUES (4, 1000 * 100, 0);
+INSERT INTO 'user' ('id', 'limite', 'saldo') VALUES (5, 1000 * 100, 0);
