b0a46923eb36a5f92ee9ce3bab461853c6e8b3e9
Daniel Morello <danielmorello@gmail.com>
2024-02-24 17:24:03 -0300

    alteração do tipo da chave primária no repositório do users

--- src/main/java/br/com/dsm/RinhaBackend/domain/user/repository/UserRepository.java
+++ src/main/java/br/com/dsm/RinhaBackend/domain/user/repository/UserRepository.java
@@ -1,9 +1,8 @@
 package br.com.dsm.RinhaBackend.domain.user.repository;
 
 import br.com.dsm.RinhaBackend.domain.user.model.User;
-import jakarta.persistence.Id;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Id> {}
+public interface UserRepository extends JpaRepository<User, Integer> {}
