6ce454764b677664e740beb7d50d52b443a46caa
Daniel Morello <danielmorello@gmail.com>
2024-02-24 17:21:05 -0300

    inclusão no endpoint da variável para o is do user

--- src/main/java/br/com/dsm/RinhaBackend/application/rest/controller/TransactionController.java
+++ src/main/java/br/com/dsm/RinhaBackend/application/rest/controller/TransactionController.java
@@ -3,6 +3,7 @@ package br.com.dsm.RinhaBackend.application.rest.controller;
 import br.com.dsm.RinhaBackend.domain.transaction.dto.TransactionDto;
 import br.com.dsm.RinhaBackend.domain.transaction.ports.inbound.CreateTransactionUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/transacao")
 public class TransactionController {
-  @Autowired
-  private CreateTransactionUseCase createTransactionUseCase;
 
-  @PostMapping
-  public void createTransaction(@RequestBody TransactionDto transactionDto) {
-    createTransactionUseCase.createTransaction(transactionDto);
-  }
+	@Autowired
+	private CreateTransactionUseCase createTransactionUseCase;
+
+	@PostMapping("/{clientId}")
+	public void createTransaction(
+		@PathVariable("clientId") Integer clientId,
+		@RequestBody TransactionDto transactionDto
+	) {
+		createTransactionUseCase.createTransaction(clientId, transactionDto);
+	}
 }
