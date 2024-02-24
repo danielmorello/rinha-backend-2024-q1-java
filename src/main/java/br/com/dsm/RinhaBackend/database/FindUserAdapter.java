21c9de2eb182bf73106a38cc885d19ad6853b66c
Daniel Morello <danielmorello@gmail.com>
2024-02-24 17:20:33 -0300

    nova classe para procurar users no banco

---
+++ src/main/java/br/com/dsm/RinhaBackend/database/FindUserAdapter.java
@@ -0,0 +1,20 @@
+package br.com.dsm.RinhaBackend.database;
+
+import br.com.dsm.RinhaBackend.domain.user.model.User;
+import br.com.dsm.RinhaBackend.domain.user.ports.outbound.FindUserPort;
+import br.com.dsm.RinhaBackend.domain.user.repository.UserRepository;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FindUserAdapter implements FindUserPort {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	public Optional<User> findUser(Integer clientId) {
+		return userRepository.findById(clientId);
+	}
+}
