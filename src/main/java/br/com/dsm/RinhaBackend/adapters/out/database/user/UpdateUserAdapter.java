31e58d6b7a33476a01c08220813b8d0c7fa3e655
Daniel Morello <danielmorello@gmail.com>
2024-02-26 16:58:34 -0300

    correção na classe para atualizar informações do user

--- src/main/java/br/com/dsm/RinhaBackend/adapters/out/database/user/UpdateUserAdapter.java
+++ src/main/java/br/com/dsm/RinhaBackend/adapters/out/database/user/UpdateUserAdapter.java
@@ -1,13 +1,20 @@
 package br.com.dsm.RinhaBackend.adapters.out.database.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import br.com.dsm.RinhaBackend.domain.user.model.User;
 import br.com.dsm.RinhaBackend.domain.user.ports.outbound.UpdateUserPort;
+import br.com.dsm.RinhaBackend.domain.user.repository.UserRepository;
 
-public class UpdateUserAdapter implements UpdateUserPort{
+@Component
+public class UpdateUserAdapter implements UpdateUserPort {
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
-    public User updateUser(User oldUser, User user) {
-        User userOld = 
+    public User updateUser(User user) {
+        return userRepository.save(user);
     }
-    
+
 }
