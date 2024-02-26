2fc5748013556f281e6000a9c0ec07bdd28c7588
Daniel Morello <danielmorello@gmail.com>
2024-02-26 19:51:14 -0300

    delete de classe que não vão ser usadas

--- src/main/java/br/com/dsm/RinhaBackend/domain/statement/ports/outbound/CreateStatementPort.java
+++
@@ -1,8 +0,0 @@
-package br.com.dsm.RinhaBackend.domain.statement.ports.outbound;
-
-import br.com.dsm.RinhaBackend.domain.statement.model.Statement;
-
-public interface CreateStatementPort {
-	// TODO: verificar a adaptação para a classe de extrato
-	void createStatement(Statement statement);
-}
