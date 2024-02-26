17e320bd848d4cb7796150826cf7e4d2d4ca11e6
Daniel Morello <danielmorello@gmail.com>
2024-02-26 16:58:01 -0300

    Alteração do tipo da rota para Put

--- src/main/java/br/com/dsm/RinhaBackend/adapters/in/controller/user/UserController.java
+++ src/main/java/br/com/dsm/RinhaBackend/adapters/in/controller/user/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,11 +38,10 @@ public class UserController {
 		return ResponseEntity.ok(userDto);
 	}
 
-	@PostMapping("/{clientId}")
+	@PutMapping("/{clientId}")
 	public ResponseEntity<UserDto> updateUser(
-		@PathVariable("clientId") Integer clientId,
-		@RequestBody UserDto userDto
-	) {
+			@PathVariable("clientId") Integer clientId,
+			@RequestBody UserDto userDto) {
 		UserDto userDtoCreated = updateUserUseCase.updateUser(clientId, userDto);
 
 		return ResponseEntity.ok(userDtoCreated);
