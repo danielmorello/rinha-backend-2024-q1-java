9a14bd4d2b56fb59fe03818a2eade6e90231bb7a
Daniel Morello <danielmorello@gmail.com>
2024-02-26 17:29:49 -0300

    nova exception para tratar que os valores das transações devem ser inteiros positivos

---
+++ src/main/java/br/com/dsm/RinhaBackend/domain/transaction/exception/TransactionValueException.java
@@ -0,0 +1,8 @@
+package br.com.dsm.RinhaBackend.domain.transaction.exception;
+
+public class TransactionValueException extends RuntimeException {
+
+	public TransactionValueException(String message) {
+		super(message);
+	}
+}
