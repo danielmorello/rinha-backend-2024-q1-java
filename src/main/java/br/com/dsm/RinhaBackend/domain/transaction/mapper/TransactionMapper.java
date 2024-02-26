406dbe6d0fef4aa41892a48c1a1bab0c3d5bc5c0
Daniel Morello <danielmorello@gmail.com>
2024-02-26 17:31:04 -0300

    lógicas para tratar o valor e a descrição das transações

--- src/main/java/br/com/dsm/RinhaBackend/domain/transaction/mapper/TransactionMapper.java
+++ src/main/java/br/com/dsm/RinhaBackend/domain/transaction/mapper/TransactionMapper.java
@@ -1,6 +1,8 @@
 package br.com.dsm.RinhaBackend.domain.transaction.mapper;
 
 import br.com.dsm.RinhaBackend.domain.transaction.dto.TransactionDto;
+import br.com.dsm.RinhaBackend.domain.transaction.exception.TransactionDescriptionLengthException;
+import br.com.dsm.RinhaBackend.domain.transaction.exception.TransactionValueException;
 import br.com.dsm.RinhaBackend.domain.transaction.model.Transaction;
 import org.springframework.stereotype.Component;
 
@@ -9,9 +11,17 @@ public class TransactionMapper {
 
   public Transaction toTransaction(TransactionDto transactionDto) {
     Transaction transaction = new Transaction();
-    transaction.setValor(transactionDto.getValor());
+    if (transactionDto.getValor() > 0) {
+      transaction.setValor(transactionDto.getValor());
+    } else {
+      throw new TransactionValueException("O valor da transação deve ser um inteiro positivo.");
+    }
     transaction.setTipo(transactionDto.getTipo());
-    transaction.setDescricao(transactionDto.getDescricao());
+    if (transactionDto.getDescricao().length() <= 10 && transactionDto.getDescricao().length() >= 1) {
+      transaction.setDescricao(transactionDto.getDescricao());
+    } else {
+      throw new TransactionDescriptionLengthException("O tamanho da ddescrição não esta compatível");
+    }
 
     return transaction;
   }
